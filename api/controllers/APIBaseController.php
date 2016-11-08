18dd1f17cee7f5dc716bc6b91afc49001e557c17
cuikuangye <cuikuangye@rong360.com>
2016-11-08 15:01:38 +0800

    api

--- api/controllers/APIBaseController.php
+++ api/controllers/APIBaseController.php
@@ -3,10 +3,7 @@ namespace backend\controllers;
 
 use Yii;
 use yii\rest\ActiveController;
-use yii\web\Controller;
-use yii\filters\VerbFilter;
-use yii\filters\AccessControl;
-use common\models\LoginForm;
+
 
 /**
  * Site controller
--- api/controllers/SmallLoansController.php
+++ api/controllers/SmallLoansController.php
@@ -8,7 +8,6 @@
 
 namespace backend\controllers;
 
-
 class SmallLoansController extends APIBaseController
 {
 
@@ -16,48 +15,16 @@ class SmallLoansController extends APIBaseController
     {
         header('Content-Type: application/json; charset=utf-8');
         $platform = \Yii::$app->request->getParam('platform');
-        $intPageNum = intval(\Yii::$app->request->getParam('page_num',0));
-        $intPageSize = intval(\Yii::$app->request->getParam('page_size',20));
-        $intPageNum = $intPageNum>1 ? $intPageNum : 1;
+        $intPageNum = intval(\Yii::$app->request->getParam('page_num', 0));
+        $intPageSize = intval(\Yii::$app->request->getParam('page_size', 20));
+        $intPageNum = $intPageNum > 1 ? $intPageNum : 1;
 
         $result['key'] = 'ss';
         $this->apiResponse($result);
         //valid signature , option
         //if($this->checkSignature($signature,$timestamp,$nonce)){
         //    echo $echoStr;
-       // }
+        // }
     }
 
-    /**
-     * Login action.
-     *
-     * @return string
-     */
-    public function actionLogin()
-    {
-        if (!Yii::$app->user->isGuest) {
-            return $this->goHome();
-        }
-
-        $model = new LoginForm();
-        if ($model->load(Yii::$app->request->post()) && $model->login()) {
-            return $this->goBack();
-        } else {
-            return $this->render('login', [
-                'model' => $model,
-            ]);
-        }
-    }
-
-    /**
-     * Logout action.
-     *
-     * @return string
-     */
-    public function actionLogout()
-    {
-        Yii::$app->user->logout();
-
-        return $this->goHome();
-    }
 }
